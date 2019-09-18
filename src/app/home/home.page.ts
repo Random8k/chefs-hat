7a81f95fbbe37fcb3838351401b23b137e76ed95
Cody Dunkley <redstar40k@gmail.com>
2019-09-18 00:46:35 -0500

    basic functionality

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +7,28 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  private answers = ['Parrains.', 'Walk-ons.', 'Voodoo BBQ.',
+             'LIT Pizza.', 'The Chimes.', 'Raising Canes.',
+             'Nine Dragon.', 'Buffalo Wild Wings.', 'Five Guys.',
+             'Izzos Illegal Burritos.', 'Rock-n-Sake.', 'Sonic.',
+             'Tokyo Cafe.', 'Popeyes.', 'Now is not the time to eat.',
+             'Ask again. I am undecided.', 'Pick your own place.', 'Subway.',
+             'Mellow Mushroom', 'Something out of your fridge.', 'Check the pantry?']
 
-  constructor() {}
+  constructor(private toastCtrl: ToastController) {}
+
+  giveAnswer() {
+    let answer = this.answers[Math.floor(Math.random() * this.answers.length)];
+    this.presentToast(answer);
+  }
+
+  async presentToast(answer) {
+    const toast = await this.toastCtrl.create({
+      message: answer,
+      duration: 5000,
+      position: 'top'
+    });
+    toast.present();
+  }
 
 }
